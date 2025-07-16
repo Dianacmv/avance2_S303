b58ae92dcfd6e6833bd8610e879ff2f6638c2392
Diana Matamoros <dmatamoros90333@ufide.ac.cr>
2025-07-15 19:25:52 -0600

    Clase Espalda agregada con su lógica

--- src/avance2_dianamatamoros_sc303/Espalda.java
+++ src/avance2_dianamatamoros_sc303/Espalda.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package avance2_dianamatamoros_sc303;
 
-/**
- *
- * @author Diana
- */
-public class Espalda {
-    
+public class Espalda extends Ejercicio {
+
+    // Constructor que inicializa el ejercicio de espalda con sus atributos
+    public Espalda(String nombre, int repeticiones, int series) {
+        super(nombre, repeticiones, series);
+    }
+
+    // Implementación del método obligatorio para mostrar la información del ejercicio
+    @Override
+    public void mostrarInformacion() {
+        System.out.println("Ejercicio de Espalda");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Repeticiones: " + repeticiones);
+        System.out.println("Series: " + series);
+    }
 }
