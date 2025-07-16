aa8be33383b7f452fe43d77d70f1419fced051e2
Diana Matamoros <dmatamoros90333@ufide.ac.cr>
2025-07-15 19:21:33 -0600

    Clase Brazo implementada

--- src/avance2_dianamatamoros_sc303/Brazo.java
+++ src/avance2_dianamatamoros_sc303/Brazo.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package avance2_dianamatamoros_sc303;
 
-/**
- *
- * @author Diana
- */
-public class Brazo {
-    
-}
+public class Brazo extends Ejercicio {
+
+    // Constructor que inicializa el ejercicio de brazo con sus atributos
+    public Brazo(String nombre, int repeticiones, int series) {
+        super(nombre, repeticiones, series);
+    }
+
+    // Implementación del método obligatorio para mostrar la información del ejercicio
+    @Override
+    public void mostrarInformacion() {
+        System.out.println("Ejercicio de Brazo");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Repeticiones: " + repeticiones);
+        System.out.println("Series: " + series);
+    }
+} 
--- src/avance2_dianamatamoros_sc303/Ejercicio.java
+++ src/avance2_dianamatamoros_sc303/Ejercicio.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package avance2_dianamatamoros_sc303;
 
-/**
- *
- * @author Diana
- */
-public class Ejercicio {
-    
+public abstract class Ejercicio implements Mostrable {
+    // Nombre del ejercicio
+    protected String nombre;
+    // Cantidad de repeticiones por serie
+    protected int repeticiones;
+    // Cantidad de series que debe realizar la persona usuaria
+    protected int series;
+
+    // Constructor: inicializa el ejercicio con los datos mínimos necesarios
+    public Ejercicio(String nombre, int repeticiones, int series) {
+        this.nombre = nombre;
+        this.repeticiones = repeticiones;
+        this.series = series;
+    }
+
+    // Método obligatorio que cada subclase debe implementar para mostrar los detalles del ejercicio
+    @Override
+    public abstract void mostrarInformacion();
+
+    // Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getRepeticiones() {
+        return repeticiones;
+    }
+
+    public void setRepeticiones(int repeticiones) {
+        this.repeticiones = repeticiones;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
 }
